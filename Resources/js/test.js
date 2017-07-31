1d7b532044833ca491789813c6ae836f5f271116
PeteHass94 <phassard@hotmail.co.uk>
2017-08-01 00:16:07 +0100

    day 5 adding CAI logo

--- Resources/js/test.js
+++ Resources/js/test.js
@@ -12,9 +12,11 @@ $('.js-menu').on('click', function() {
 	if (isActive) {
 		$(this).removeClass('active');
 		$('body').removeClass('menu-open');
+    $("#menuComment").text("Display Menu");
 	} else {
 		$(this).addClass('active');
 		$('body').addClass('menu-open');
+    $("#menuComment").text("Hide Menu");
 	}
 
 	isActive = !isActive;
