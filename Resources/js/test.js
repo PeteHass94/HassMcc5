ae6565570af3e6d1fc09eb4a86bba95998fa6ce7
PeteHass94 <phassard@hotmail.co.uk>
2017-07-31 23:35:08 +0100

    testing menu expantion

--- Resources/js/test.js
+++ Resources/js/test.js
@@ -1,1 +1,21 @@
 
+function displayNav(){
+  document.getElementById("hidden").style.display = "flex";
+}
+function hideNav(){
+  document.getElementById("hidden").style.display = "none";
+}
+
+var isActive = false;
+
+$('.js-menu').on('click', function() {
+	if (isActive) {
+		$(this).removeClass('active');
+		$('body').removeClass('menu-open');
+	} else {
+		$(this).addClass('active');
+		$('body').addClass('menu-open');
+	}
+
+	isActive = !isActive;
+});
